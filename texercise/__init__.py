9a9d718e21c13d6e89f82e4968da52b137b5b4d1
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2020-02-13 19:31:02 +0100

    .

--- texercise/__init__.py
+++ texercise/__init__.py
@@ -1,69 +1,103 @@
 import os
 import zipfile
 import tempfile
-import time
 from pathlib import Path
+import unicodedata
+import re
+import urllib.parse
+from typing import List, Tuple
+import sys
+import datetime
 
-import requests
+import click
 
 base_url = 'http://texercise.rasmushaugaard.dk'
 
 
-def load_config(fp: Path = None):
-    if fp is not None:
-        try:
-            with fp.open() as f:
-                course, email, token = [l.strip() for l in f.readlines()]
-        except Exception as e:
-            print("Could not read .texercise file")
-            raise e
-        return course, email, token
+def validate_email(ctx, param, value):
+    if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", value):
+        raise click.UsageError('Incorrect email address given')
+    else:
+        return value
 
-    folder = Path()
+
+def url_quote(inp):
+    if isinstance(inp, (List, Tuple)):
+        return [url_quote(a) for a in inp]
+    return urllib.parse.quote(inp)
+
+
+class Context:
+    NONE = None
+    COURSE = 'course'
+    EXERCISE = 'exercise'
+
+
+def _load_config(fp: Path):
+    try:
+        with fp.open() as f:
+            course, user_type, email, token = [l.strip() for l in f.readlines()]
+    except Exception as e:
+        print("Could not read .texercise file")
+        raise e
+    return course, user_type, email, token
+
+
+def load_config():
+    folder = Path().absolute()
     fp = folder / ".texercise"
     if fp.exists():
-        return "course", load_config(fp)
-    fp = folder.parent / ".texercise"
-    if fp.exists():
-        return "exercise", load_config(fp)
-    return None, (None, None, None)
+        return "course", folder, None, _load_config(fp)
+    while folder.parent != folder:
+        child_folder = folder
+        folder = folder.parent
+        fp = folder / ".texercise"
+        if fp.exists():
+            return "exercise", folder, child_folder, _load_config(fp)
+    return None, None, None, (None, None, None, None)
 
 
-def zipdir(dir: Path):
+def zipdir(dir: Path, max_size_kb=0, size_e_message='File too large'):
     tmp = tempfile.mktemp(prefix="texercise-", suffix=".zip")
     zipf = zipfile.ZipFile(tmp, 'w', zipfile.ZIP_DEFLATED)
     for root, dirs, files in os.walk(str(dir.absolute())):
         for file in files:
-            zipf.write(os.path.join(root, file))
+            fp = Path(root) / file
+            zipf.write(fp, fp.relative_to(dir))
     zipf.close()
+    if max_size_kb:
+        size_kb = os.stat(tmp).st_size / 1024
+        if size_kb > max_size_kb:
+            print(size_e_message, '{}/{} kB'.format(int(size_kb), int(max_size_kb)))
+            sys.exit()
     return tmp
 
 
-def main():
-    z = zipdir()
-    r = requests.post(
-        url='{}/upload'.format(base_url),
-        files={'archive': open(z, 'rb')},
-    )
-    response_data = r.json()
-    print(response_data)
-    job_id = response_data['data']['job_id']
-    print(job_id)
-    return job_id
+def valid_filesystem_name(value):
+    """
+    Normalizes string, converts to lowercase, removes non-alpha characters,
+    and converts spaces to hyphens.
+    """
+    value = unicodedata.normalize('NFKD', value)
+    value = re.sub('[^\w\s-]', '', value).strip().lower()
+    value = re.sub('[-\s]+', '-', value)
+    return value
 
 
-if __name__ == '__main__':
-    job_id = main()
-    while True:
-        r = requests.get("{}/jobs/{}".format(base_url, job_id))
-        data = r.json()['data']
-        job_status = data['job_status']
-        if job_status == 'queued':
-            print("queued")
-        elif job_status == 'started':
-            print("Currently checking your solution")
-        else:
-            print("job_status", job_status)
-            break
-        time.sleep(1)
-    print(data['job_result'])
+def duration_format(s):
+    M = 60
+    H = M * 60
+    D = H * 24
+    days = int(s // D)
+    s -= days * D
+    hours = int(s // H)
+    s -= hours * H
+    minutes = int(s // M)
+    s -= minutes * M
+    seconds = int(s // 1)
+    milliseconds = int((s - seconds) * 1000)
+    return '{:2d}d {:2d}h {:2d}m {:2d}.{:3d}s'.format(days, hours, minutes, seconds, milliseconds)
+
+
+def get_echo_exercise_folder():
+    return Path(__file__).parent / 'echo_exercise'
