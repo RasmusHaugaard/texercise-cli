6f8aaec971eeebb93611d7cd3836dcef3259d3ee
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2020-02-15 14:31:39 +0100

    fixed autoupdate

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 import setuptools
 from texercise import __version__
+from pathlib import Path
+
+texercise_folder = Path('texercise')
+echo_exercise_files = texercise_folder.glob('echo_exercise/**/*')
 
 setuptools.setup(
     name='texercise',
@@ -12,6 +16,9 @@ setuptools.setup(
         'bin/texercise',
     ],
     packages=setuptools.find_packages(),
+    package_data={
+        'texercise': [str(p.relative_to(texercise_folder)) for p in echo_exercise_files]
+    },
     install_requires=[
         'request',
         'click',
