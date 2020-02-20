63ae879fa7afd581759ea30d9d6c6b4970239a40
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2020-02-20 15:48:38 +0100

    added ignore globs, in memory zip file

--- texercise/__init__.py
+++ texercise/__init__.py
@@ -7,16 +7,43 @@ import re
 import urllib.parse
 from typing import List, Tuple
 import sys
-from .version import __version__
+import io
 
 import requests
 import click
+
+from .version import __version__
 
 
 def get_latest_version_str():
     r = requests.get('https://pypi.python.org/pypi/texercise/json')
     response = r.json()
     return response['info']['version']
+
+
+upload_ignore = (
+    '*/.*',
+    '*/CMakeFiles',
+    '*/Makefile',
+    '*/cmake_install.cmake',
+    '*/CMakeCache.txt',
+    '*.o',
+)
+
+
+def fn_match_recursive(fp: Path, ignores, root: Path = None):
+    if root is None:
+        root = fp
+    rel_fp = Path("/" + str(fp.relative_to(root)))
+    if any(rel_fp.match(ignore) for ignore in ignores):
+        return []
+    elif fp.is_file():
+        return [fp]
+    elif fp.is_dir():
+        item_lists = (fn_match_recursive(it, ignores, root) for it in fp.glob("*"))
+        return (item for sublist in item_lists for item in sublist)
+    else:
+        return []
 
 
 def parse_version(version_str):
@@ -27,7 +54,7 @@ def parse_version(version_str):
 base_url = 'http://texercise.rasmushaugaard.dk'
 
 
-def validate_email(ctx, param, value):
+def validate_email(_, __, value):
     if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", value):
         raise click.UsageError('Incorrect email address given')
     else:
@@ -70,20 +97,18 @@ def load_config():
     return None, None, None, (None, None, None, None)
 
 
-def zipdir(dir: Path, max_size_kb=0, size_e_message='File too large'):
-    tmp = tempfile.mktemp(prefix="texercise-", suffix=".zip")
-    zipf = zipfile.ZipFile(tmp, 'w', zipfile.ZIP_DEFLATED)
-    for root, dirs, files in os.walk(str(dir.absolute())):
-        for file in files:
-            fp = Path(root) / file
-            zipf.write(fp, fp.relative_to(dir))
-    zipf.close()
+def zip_files(files: List[Path], root: Path, max_size_kb=0, size_e_message='File too large'):
+    zip_buffer = io.BytesIO()
+    with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zf:
+        for fp in files:
+            zf.write(fp, fp.relative_to(root))
     if max_size_kb:
-        size_kb = os.stat(tmp).st_size / 1024
+        size_kb = zip_buffer.getbuffer().nbytes / 1024
         if size_kb > max_size_kb:
             print(size_e_message, '{}/{} kB'.format(int(size_kb), int(max_size_kb)))
             sys.exit()
-    return tmp
+    zip_buffer.seek(0)
+    return zip_buffer
 
 
 def valid_filesystem_name(value):
